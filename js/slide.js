21b5cc1f53f0d47169277d5252ce513059036989
João Xarrua <joaoxarrua137@gmail.com>
2022-11-23 08:31:56 -0300

    custom controls estilizados

--- js/slide.js
+++ js/slide.js
@@ -164,7 +164,7 @@ export class Slide {
   }
 }
 
-export class Slidenav extends Slide {
+export default class Slidenav extends Slide {
   constructor(slide, wrapper) {
     super(slide, wrapper);
 
